94a69a411750350988b78cca6bb8d664217dc95c
Cauã <caualeu2016@gmail.com>
2025-09-20 16:37:33 -0300

    adiciona campo category à classe TransactionResponseDTO para incluir informações da categoria nas respostas de transação

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/TransactionResponseDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/TransactionResponseDTO.java
@@ -1,18 +1,19 @@
 package com.finance.app.pfm_full_stack_backend.dto.transaction;
 
+import com.finance.app.pfm_full_stack_backend.dto.category.CategoryInfoDTO;
 import com.finance.app.pfm_full_stack_backend.entity.Transaction;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-
 public record TransactionResponseDTO(
         UUID id,
         String description,
         BigDecimal amount,
         LocalDate date,
-        Transaction.TransactionType type
+        Transaction.TransactionType type,
+        CategoryInfoDTO category
 )
 {
     public TransactionResponseDTO(Transaction transaction)
@@ -22,7 +23,8 @@ public record TransactionResponseDTO(
                 transaction.getDescription(),
                 transaction.getAmount(),
                 transaction.getDate(),
-                transaction.getType()
+                transaction.getType(),
+                transaction.getCategory() != null ? new CategoryInfoDTO(transaction.getCategory()) : null
         );
     }
 }
