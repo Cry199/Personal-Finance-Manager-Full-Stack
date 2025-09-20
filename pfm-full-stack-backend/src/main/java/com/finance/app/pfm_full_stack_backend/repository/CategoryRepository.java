ae7a37cbc75979645d80c1d9f8bb156b85628053
Cauã <caualeu2016@gmail.com>
2025-09-20 16:20:17 -0300

    adiciona a interface CategoryRepository estendendo JpaRepository e implementa método para buscar categorias por ID de usuário

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/repository/CategoryRepository.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/repository/CategoryRepository.java
@@ -1,4 +1,12 @@
 package com.finance.app.pfm_full_stack_backend.repository;
 
-public interface CategoryRepository {
+import com.finance.app.pfm_full_stack_backend.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID>
+{
+    List<Category> findAllByUserId(UUID userId);
 }
