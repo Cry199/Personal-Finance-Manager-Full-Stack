ce25d68a93f617a0e192d9d4487972dfae1b7597
Cauã <caualeu2016@gmail.com>
2025-09-20 17:01:08 -0300

    adiciona método para obter o resumo do dashboard para o mês atual

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/service/dashboard/DashboardService.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/service/dashboard/DashboardService.java
@@ -1,4 +1,47 @@
 package com.finance.app.pfm_full_stack_backend.service.dashboard;
 
-public class DashboardService {
+import com.finance.app.pfm_full_stack_backend.dto.dashboard.DashboardSummaryDTO;
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+import com.finance.app.pfm_full_stack_backend.entity.User;
+import com.finance.app.pfm_full_stack_backend.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+@Service
+public class DashboardService
+{
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public DashboardSummaryDTO getDashboardSummaryForCurrentMonth()
+    {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        LocalDate today = LocalDate.now();
+        LocalDate startDate = today.with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate endDate = today.with(TemporalAdjusters.lastDayOfMonth());
+
+        BigDecimal totalIncome = transactionRepository.sumAmountByTypeAndDateRange(
+                user.getId(),
+                Transaction.TransactionType.INCOME,
+                startDate,
+                endDate
+        );
+
+        BigDecimal totalExpense = transactionRepository.sumAmountByTypeAndDateRange(
+                user.getId(),
+                Transaction.TransactionType.EXPENSE,
+                startDate,
+                endDate
+        );
+
+        BigDecimal balance = totalIncome.subtract(totalExpense);
+
+        return new DashboardSummaryDTO(totalIncome, totalExpense, balance);
+    }
 }
