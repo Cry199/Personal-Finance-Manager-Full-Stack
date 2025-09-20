91d876e91879232e7b3734e9eed6b7e5fb605445
Cauã <caualeu2016@gmail.com>
2025-09-20 15:49:57 -0300

    adiciona campos de descrição, valor, data e tipo à classe CreateTransactionDTO

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/CreateTransactionDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/CreateTransactionDTO.java
@@ -1,4 +1,13 @@
 package com.finance.app.pfm_full_stack_backend.dto.transaction;
 
-public record CreateTransactionDTO() {
-}
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CreateTransactionDTO(
+        String description,
+        BigDecimal amount,
+        LocalDate date,
+        Transaction.TransactionType type
+) { }
