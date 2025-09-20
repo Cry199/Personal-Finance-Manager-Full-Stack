5be601088990fba68e2e48da8e9c68acf0774926
Cauã <caualeu2016@gmail.com>
2025-09-20 15:50:05 -0300

    adiciona implementação da interface TransactionRepository com métodos para manipulação de transações

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/repository/TransactionRepository.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/repository/TransactionRepository.java
@@ -1,4 +1,12 @@
 package com.finance.app.pfm_full_stack_backend.repository;
 
-public interface TransactionRepository {
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID>
+{
+    List<Transaction> findAllByUserId(UUID userId);
 }
