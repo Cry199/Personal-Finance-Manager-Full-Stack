878bb936eed36616b2051e45e01655ac10a58241
Cauã <caualeu2016@gmail.com>
2025-09-20 16:37:29 -0300

    adiciona campo categoryId à classe CreateTransactionDTO para associar transações a categorias

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/CreateTransactionDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/CreateTransactionDTO.java
@@ -4,10 +4,12 @@ import com.finance.app.pfm_full_stack_backend.entity.Transaction;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record CreateTransactionDTO(
         String description,
         BigDecimal amount,
         LocalDate date,
-        Transaction.TransactionType type
+        Transaction.TransactionType type,
+        UUID categoryId
 ) { }
