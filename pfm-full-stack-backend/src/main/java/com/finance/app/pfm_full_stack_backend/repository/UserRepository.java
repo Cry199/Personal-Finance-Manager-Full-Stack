dc87752076c0adbebc8e239ec8148fa24f426256
Cauã <caualeu2016@gmail.com>
2025-09-20 15:38:55 -0300

    adiciona implementação do UserRepository estendendo JpaRepository e método para buscar usuário por email

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/repository/UserRepository.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.finance.app.pfm_full_stack_backend.repository;
 
-public interface UserRepository {
+import com.finance.app.pfm_full_stack_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>
+{
+    UserDetails findByEmail(String email);
 }
