1069a48df735edb6752a5b89cca9c43323b2f096
Cauã <caualeu2016@gmail.com>
2025-09-20 17:18:38 -0300

    adiciona classes RestErrorMessageDTO e RestExceptionHandler para tratamento de exceções

---
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/exception/RestErrorMessageDTO.java
@@ -0,0 +1,4 @@
+package com.finance.app.pfm_full_stack_backend.dto.exception;
+
+public class RestErrorMessageDTO {
+}
---
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/exception/RestExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.finance.app.pfm_full_stack_backend.exception;
+
+public class RestExceptionHandler {
+}
