3477197d848960a19e9ee377a8df8e7fd89f6430
Cauã <caualeu2016@gmail.com>
2025-09-20 14:26:06 -0300

    adiciona estrutura inicial do projeto com arquivos de configuração do Maven e classes principais

---
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/PfmFullStackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finance.app.pfm_full_stack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmFullStackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmFullStackBackendApplication.class, args);
+	}
+
+}
---
+++ pfm-full-stack-backend/src/test/java/com/finance/app/pfm_full_stack_backend/PfmFullStackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.app.pfm_full_stack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmFullStackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
