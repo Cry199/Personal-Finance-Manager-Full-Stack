7eb1128e10f37f910affa20fe1dd82c5e9894769
Cauã <caualeu2016@gmail.com>
2025-09-20 16:40:04 -0300

    adiciona validação para o campo description na classe CreateTransactionDTO

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/CreateTransactionDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/CreateTransactionDTO.java
@@ -1,15 +1,27 @@
 package com.finance.app.pfm_full_stack_backend.dto.transaction;
 
 import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CreateTransactionDTO(
+        @NotBlank
         String description,
+
+        @NotNull
+        @Positive
         BigDecimal amount,
+
+        @NotNull
         LocalDate date,
+
+        @NotNull
         Transaction.TransactionType type,
+
         UUID categoryId
 ) { }
