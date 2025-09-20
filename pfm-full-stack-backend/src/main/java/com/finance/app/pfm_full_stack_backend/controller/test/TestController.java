05be85f64d1d9df17c2705581134661dc4e2c1cd
Cauã <caualeu2016@gmail.com>
2025-09-20 15:38:29 -0300

    adiciona controlador de teste com endpoint para validação de token

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/test/TestController.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/test/TestController.java
@@ -1,4 +1,17 @@
 package com.finance.app.pfm_full_stack_backend.controller.test;
 
-public class TestController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController
+{
+    @GetMapping
+    public ResponseEntity<String> getTestData()
+    {
+        return ResponseEntity.ok("Se você está vendo isso, seu token é válido!");
+    }
 }
