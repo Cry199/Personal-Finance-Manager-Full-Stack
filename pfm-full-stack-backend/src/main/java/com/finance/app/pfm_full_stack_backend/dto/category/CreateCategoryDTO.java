68bbefb924423d19cf7f1700c6bf5f945d6cdc8b
Cauã <caualeu2016@gmail.com>
2025-09-20 16:37:24 -0300

    remove campos de transação da classe CreateCategoryDTO e restaura a definição original

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/category/CreateCategoryDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/category/CreateCategoryDTO.java
@@ -1,16 +1,3 @@
 package com.finance.app.pfm_full_stack_backend.dto.category;
 
-import com.finance.app.pfm_full_stack_backend.entity.Transaction;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.UUID;
-
-public record CreateTransactionDTO(
-        String description,
-        BigDecimal amount,
-        LocalDate date,
-        Transaction.TransactionType type,
-        UUID categoryId
-) {
-}
+public record CreateCategoryDTO(String name) { }
