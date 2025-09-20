b3bb03494f7da1a340a39d1757d7fd03933c59b1
Cauã <caualeu2016@gmail.com>
2025-09-20 16:37:06 -0300

    refatora CategoryController para incluir anotações REST e métodos de manipulação de categorias

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/category/CategoryController.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/category/CategoryController.java
@@ -1,4 +1,39 @@
 package com.finance.app.pfm_full_stack_backend.controller.category;
 
-public class CategoryController {
+
+import com.finance.app.pfm_full_stack_backend.dto.category.CreateCategoryDTO;
+import com.finance.app.pfm_full_stack_backend.entity.Category;
+import com.finance.app.pfm_full_stack_backend.service.category.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController
+{
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody CreateCategoryDTO data)
+    {
+        Category newCategory = categoryService.createCategory(data);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newCategory.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories()
+    {
+        List<Category> categories = categoryService.getCategoriesForUser();
+        return ResponseEntity.ok(categories);
+    }
 }
