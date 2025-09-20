31cfcc0fd8857310e9cbe111d20894f590beacaf
Cauã <caualeu2016@gmail.com>
2025-09-20 15:49:51 -0300

    adiciona implementação do TransactionController com método para criar transações

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/transaction/TransactionController.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/transaction/TransactionController.java
@@ -1,4 +1,33 @@
 package com.finance.app.pfm_full_stack_backend.controller.transaction;
 
-public class TransactionController {
+import com.finance.app.pfm_full_stack_backend.dto.transaction.CreateTransactionDTO;
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+import com.finance.app.pfm_full_stack_backend.service.transaction.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController
+{
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody CreateTransactionDTO data)
+    {
+        Transaction newTransaction = transactionService.createTransaction(data);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newTransaction.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newTransaction);
+    }
 }
