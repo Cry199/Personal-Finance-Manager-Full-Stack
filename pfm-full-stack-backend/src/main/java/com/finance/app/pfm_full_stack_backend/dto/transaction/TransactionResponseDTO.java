53512c5530f16722f48fe0487d7990d81a779d0a
Cauã <caualeu2016@gmail.com>
2025-09-20 16:10:03 -0300

    adiciona a classe TransactionResponseDTO para encapsular dados de transações

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/TransactionResponseDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/transaction/TransactionResponseDTO.java
@@ -1,4 +1,28 @@
 package com.finance.app.pfm_full_stack_backend.dto.transaction;
 
-public record TransactionResponseDTO() {
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+public record TransactionResponseDTO(
+        UUID id,
+        String description,
+        BigDecimal amount,
+        LocalDate date,
+        Transaction.TransactionType type
+)
+{
+    public TransactionResponseDTO(Transaction transaction)
+    {
+        this(
+                transaction.getId(),
+                transaction.getDescription(),
+                transaction.getAmount(),
+                transaction.getDate(),
+                transaction.getType()
+        );
+    }
 }
