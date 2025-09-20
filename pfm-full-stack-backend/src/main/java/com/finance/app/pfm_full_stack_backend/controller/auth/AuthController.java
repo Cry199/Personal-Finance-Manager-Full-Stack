6943bc94d8596e38aa71bdb4ca3bdd15ba916138
Cauã <caualeu2016@gmail.com>
2025-09-20 15:38:18 -0300

    adiciona controlador de autenticação com métodos de login e registro

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/auth/AuthController.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/auth/AuthController.java
@@ -1,7 +1,61 @@
-package com.finance.app.pfm_full_stack_backend.controller;
+package com.finance.app.pfm_full_stack_backend.controller.auth;
 
+import com.finance.app.pfm_full_stack_backend.dto.auth.LoginDTO;
+import com.finance.app.pfm_full_stack_backend.dto.auth.LoginResponseDTO;
+import com.finance.app.pfm_full_stack_backend.dto.auth.RegisterDTO;
+import com.finance.app.pfm_full_stack_backend.entity.User;
+import com.finance.app.pfm_full_stack_backend.repository.UserRepository;
+import com.finance.app.pfm_full_stack_backend.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/auth")
 public class AuthController
 {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO data)
+    {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterDTO data)
+    {
+        if (this.userRepository.findByEmail(data.email()) != null)
+        {
+            return ResponseEntity.badRequest().body("Email já está em uso.");
+        }
+
+        String encryptedPassword = passwordEncoder.encode(data.password());
+        User newUser = new User(null, data.name(), data.email(), encryptedPassword);
+
+        this.userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
 }
