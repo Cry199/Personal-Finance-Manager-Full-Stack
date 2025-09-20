97202d5d26faa2bf501bb21c9fd2b0dc4eead9e0
Cauã <caualeu2016@gmail.com>
2025-09-20 18:51:54 -0300

    adiciona DTO para atualização de transações recorrentes com validações

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/recurring/UpdateRecurringTransactionDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/recurring/UpdateRecurringTransactionDTO.java
@@ -1,4 +1,19 @@
 package com.finance.app.pfm_full_stack_backend.dto.recurring;
 
-public class UpdateRecurringTransactionDTO {
-}
+import com.finance.app.pfm_full_stack_backend.entity.RecurringTransaction;
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record UpdateRecurringTransactionDTO(
+        String description,
+        @Positive BigDecimal amount,
+        Transaction.TransactionType type,
+        LocalDate startDate,
+        LocalDate endDate,
+        RecurringTransaction.RecurrencePeriod period,
+        UUID categoryId
+) { }
