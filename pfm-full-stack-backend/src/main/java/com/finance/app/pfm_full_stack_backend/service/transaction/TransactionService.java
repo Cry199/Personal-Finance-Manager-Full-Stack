9e22146152e17f4cefa6fcde43f8515cc5535b47
Cauã <caualeu2016@gmail.com>
2025-09-20 15:50:14 -0300

    adiciona implementação do serviço de transações com método para criar transações

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/service/transaction/TransactionService.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/service/transaction/TransactionService.java
@@ -1,4 +1,34 @@
 package com.finance.app.pfm_full_stack_backend.service.transaction;
 
-public class TransactionService {
+import com.finance.app.pfm_full_stack_backend.dto.transaction.CreateTransactionDTO;
+import com.finance.app.pfm_full_stack_backend.entity.Transaction;
+import com.finance.app.pfm_full_stack_backend.entity.User;
+import com.finance.app.pfm_full_stack_backend.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService
+{
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Transaction createTransaction(CreateTransactionDTO data)
+    {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Transaction newTransaction = new Transaction(
+                null,
+                data.description(),
+                data.amount(),
+                data.date(),
+                data.type(),
+                user
+        );
+
+        return transactionRepository.save(newTransaction);
+    }
+
+    // listar, atualizar e deletar
 }
