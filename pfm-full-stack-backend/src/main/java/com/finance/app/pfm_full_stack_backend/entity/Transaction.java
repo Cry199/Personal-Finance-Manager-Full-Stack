6cf966a4769ea9edaa7e5dc2201329c4540e950c
Cauã <caualeu2016@gmail.com>
2025-09-20 15:49:45 -0300

    adiciona implementação inicial das classes TransactionController, CreateTransactionDTO, Transaction, TransactionRepository, AuthService e TransactionService

---
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/entity/Transaction.java
@@ -0,0 +1,44 @@
+package com.finance.app.pfm_full_stack_backend.controller.test;
+
+import com.finance.app.pfm_full_stack_backend.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity(name = "Transaction")
+@Table(name = "transactions")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private BigDecimal amount;
+
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public enum TransactionType
+    {
+        INCOME, // Receita
+        EXPENSE // Despesa
+    }
+}
