178f0c7e0363a7340f4495958253c5d53533fdca
Cauã <caualeu2016@gmail.com>
2025-09-20 16:20:12 -0300

    adiciona a classe Category com anotações JPA e Lombok para mapeamento e simplificação

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/entity/Category.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/entity/Category.java
@@ -1,4 +1,28 @@
 package com.finance.app.pfm_full_stack_backend.entity;
 
-public class Category {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity(name = "Category")
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Category
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
