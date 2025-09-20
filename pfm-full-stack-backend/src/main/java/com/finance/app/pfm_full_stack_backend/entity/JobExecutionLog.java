bdd37e5dfd23e8c131778ddf05c22bbb1be026ef
Cauã <caualeu2016@gmail.com>
2025-09-20 18:14:26 -0300

    adiciona a classe JobExecutionLog com anotações JPA e Lombok

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/entity/JobExecutionLog.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/entity/JobExecutionLog.java
@@ -1,4 +1,25 @@
 package com.finance.app.pfm_full_stack_backend.entity;
 
-public class JobExecutionLog {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "job_execution_log")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobExecutionLog
+{
+    @Id
+    private String jobName;
+
+    private LocalDateTime lastExecution;
 }
