52d70e5f958a6743f1dda6cfd14b78435414b52b
Cauã <caualeu2016@gmail.com>
2025-09-20 17:00:48 -0300

    adiciona a classe DashboardController com o endpoint para obter o resumo do dashboard

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/dashboard/DashboardController.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/controller/dashboard/DashboardController.java
@@ -1,4 +1,24 @@
 package com.finance.app.pfm_full_stack_backend.controller.dashboard;
 
-public class DashboardController {
+import com.finance.app.pfm_full_stack_backend.dto.dashboard.DashboardSummaryDTO;
+import com.finance.app.pfm_full_stack_backend.service.dashboard.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController
+{
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("/summary")
+    public ResponseEntity<DashboardSummaryDTO> getDashboardSummary()
+    {
+        DashboardSummaryDTO summary = dashboardService.getDashboardSummaryForCurrentMonth();
+        return ResponseEntity.ok(summary);
+    }
 }
