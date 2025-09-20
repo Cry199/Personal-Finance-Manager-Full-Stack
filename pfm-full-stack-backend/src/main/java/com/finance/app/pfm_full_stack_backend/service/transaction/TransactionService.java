b6abb1650701081ac477296fae5735a738b70684
Cauã <caualeu2016@gmail.com>
2025-09-20 15:56:47 -0300

    adiciona método para atualizar transações na classe TransactionService

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/service/transaction/TransactionService.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/service/transaction/TransactionService.java
@@ -1,6 +1,7 @@
 package com.finance.app.pfm_full_stack_backend.service.transaction;
 
 import com.finance.app.pfm_full_stack_backend.dto.transaction.CreateTransactionDTO;
+import com.finance.app.pfm_full_stack_backend.dto.transaction.UpdateTransactionDTO;
 import com.finance.app.pfm_full_stack_backend.entity.Transaction;
 import com.finance.app.pfm_full_stack_backend.entity.User;
 import com.finance.app.pfm_full_stack_backend.repository.TransactionRepository;
@@ -9,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TransactionService
@@ -37,5 +39,37 @@ public class TransactionService
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         return transactionRepository.findAllByUserId(user.getId());
+    }
+
+    public Transaction updateTransaction(UUID id, UpdateTransactionDTO data)
+    {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        var transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Transação não encontrada"));
+
+
+        if (!transaction.getUser().getId().equals(user.getId()))
+        {
+            throw new SecurityException("Acesso negado: você não é o dono desta transação.");
+        }
+
+        if (data.description() != null)
+        {
+            transaction.setDescription(data.description());
+        }
+        if (data.amount() != null)
+        {
+            transaction.setAmount(data.amount());
+        }
+        if (data.date() != null)
+        {
+            transaction.setDate(data.date());
+        }
+        if (data.type() != null) {
+            transaction.setType(data.type());
+        }
+
+        return transactionRepository.save(transaction);
     }
 }
