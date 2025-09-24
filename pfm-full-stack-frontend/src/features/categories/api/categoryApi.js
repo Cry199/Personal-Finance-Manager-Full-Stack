7cd49dca6cd40c5afb79144a989d78dbb08fa34b
Cauã <caualeu2016@gmail.com>
2025-09-24 19:02:18 -0300

    feat: adiciona quebras de linha e formatação no arquivo categoryApi.js

--- pfm-full-stack-frontend/src/features/categories/api/categoryApi.js
+++ pfm-full-stack-frontend/src/features/categories/api/categoryApi.js
@@ -1,5 +1,11 @@
 import api from '../../../api/axiosConfig';
 
+// Função para buscar todas as categorias
 export const getCategories = () => {
   return api.get('/categories');
+};
+
+// Função para criar uma nova categoria
+export const createCategory = (categoryData) => {
+  return api.post('/categories', categoryData);
 };
