3fef0b8c1cb6ed3b43ce803155732768aa2f5de2
Cauã <caualeu2016@gmail.com>
2025-09-27 01:22:19 -0300

    feat: adiciona rota para a página de transações recorrentes

--- pfm-full-stack-frontend/src/App.jsx
+++ pfm-full-stack-frontend/src/App.jsx
@@ -7,6 +7,7 @@ import DashboardPage from './features/dashboard/pages/DashboardPage';
 import TransactionsPage from './features/transactions/pages/TransactionsPage';
 import MainLayout from './components/layout/MainLayout';
 import CategoriesPage from './features/categories/pages/CategoriesPage';
+import RecurringTransactionsPage from './features/recurring/pages/RecurringTransactionsPage';
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -38,6 +39,7 @@ const AppContent = () => {
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="transactions" element={<TransactionsPage />} />
           <Route path="categories" element={<CategoriesPage />} />
+          <Route path="recurring-transactions" element={<RecurringTransactionsPage />} />
           {/* Redirecionar a rota raiz para o dashboard */}
           <Route index element={<Navigate to="/dashboard" />} />
         </Route>
