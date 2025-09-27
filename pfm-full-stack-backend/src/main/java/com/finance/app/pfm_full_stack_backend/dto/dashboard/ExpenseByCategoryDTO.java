a6d785e2e9cfd44817aad158b142e154190f2c95
Cauã <caualeu2016@gmail.com>
2025-09-27 01:38:54 -0300

    feat: adiciona campos categoryName e totalAmount ao DTO ExpenseByCategory

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/dashboard/ExpenseByCategoryDTO.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/dashboard/ExpenseByCategoryDTO.java
@@ -1,4 +1,8 @@
 package com.finance.app.pfm_full_stack_backend.dto.dashboard;
 
-public record ExpenseByCategoryDTO() {
-}
+import java.math.BigDecimal;
+
+public record ExpenseByCategoryDTO(
+        String categoryName,
+        BigDecimal totalAmount
+) { }
