f08210eb9977ee4b48a5e62eaf4354adccb47259
Cauã <caualeu2016@gmail.com>
2025-09-27 01:47:11 -0300

    feat: adiciona função para obter resumo do dashboard

--- pfm-full-stack-frontend/src/features/dashboard/api/dashboardApi.js
+++ pfm-full-stack-frontend/src/features/dashboard/api/dashboardApi.js
@@ -3,3 +3,7 @@ import api from '../../../api/axiosConfig';
 export const getDashboardSummary = () => {
   return api.get('/dashboard/summary');
 };
+
+export const getExpensesByCategory = () => {
+  return api.get('/dashboard/expenses-by-category');
+};
