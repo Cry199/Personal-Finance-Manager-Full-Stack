31f61410a6510a9edcb81936067dda9cb8cccf47
Cauã <caualeu2016@gmail.com>
2025-09-29 23:41:31 -0300

    feat: adicionar tratamento de exceções personalizadas para erros de negócio

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/exception/RestExceptionHandler.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/exception/RestExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.finance.app.pfm_full_stack_backend.exception;
 
+import com.finance.app.pfm_full_stack_backend.dto.exception.BusinessException;
+import com.finance.app.pfm_full_stack_backend.dto.exception.OperationNotPermittedException;
+import com.finance.app.pfm_full_stack_backend.dto.exception.ResourceNotFoundException;
 import com.finance.app.pfm_full_stack_backend.dto.exception.RestErrorMessageDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -9,19 +12,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler
-{
-    @ExceptionHandler(RuntimeException.class)
-    private ResponseEntity<RestErrorMessageDTO> handleRuntimeException(RuntimeException exception)
-    {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    private ResponseEntity<RestErrorMessageDTO> handleResourceNotFound(ResourceNotFoundException exception) {
         RestErrorMessageDTO error = new RestErrorMessageDTO(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
-    @ExceptionHandler(SecurityException.class)
-    private ResponseEntity<RestErrorMessageDTO> handleSecurityException(SecurityException exception)
-    {
+    @ExceptionHandler(OperationNotPermittedException.class)
+    private ResponseEntity<RestErrorMessageDTO> handleOperationNotPermitted(OperationNotPermittedException exception) {
         RestErrorMessageDTO error = new RestErrorMessageDTO(HttpStatus.FORBIDDEN, exception.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    private ResponseEntity<RestErrorMessageDTO> handleBusinessException(BusinessException exception) {
+        RestErrorMessageDTO error = new RestErrorMessageDTO(HttpStatus.CONFLICT, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 }
