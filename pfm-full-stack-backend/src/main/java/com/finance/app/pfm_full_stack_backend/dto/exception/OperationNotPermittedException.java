e644e45403dba579ae930e671bbc9a2153461f52
Cauã <caualeu2016@gmail.com>
2025-09-29 23:41:18 -0300

    feat: adicionar exceção OperationNotPermittedException com status FORBIDDEN

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/exception/OperationNotPermittedException.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/exception/OperationNotPermittedException.java
@@ -1,4 +1,10 @@
 package com.finance.app.pfm_full_stack_backend.dto.exception;
 
-public class OperationNotPermittedException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class OperationNotPermittedException extends RuntimeException
+{
+    public OperationNotPermittedException(String message) { super(message); }
 }
