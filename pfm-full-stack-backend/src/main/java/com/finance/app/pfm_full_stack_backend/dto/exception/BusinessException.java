b8279ae9c2787e0af5d9465f62e3aec54761238a
Cauã <caualeu2016@gmail.com>
2025-09-29 23:41:14 -0300

    feat: implementar exceção de negócio com status de conflito

--- pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/exception/BusinessException.java
+++ pfm-full-stack-backend/src/main/java/com/finance/app/pfm_full_stack_backend/dto/exception/BusinessException.java
@@ -1,4 +1,10 @@
 package com.finance.app.pfm_full_stack_backend.dto.exception;
 
-public class BusinessException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class BusinessException extends RuntimeException
+{
+    public BusinessException(String message) { super(message); }
 }
